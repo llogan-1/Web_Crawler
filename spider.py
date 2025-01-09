5d6075a9dbd1677b386d847dcb235fd8af5a39eb
Logan Jarvis <ljarvis2@rocketmail.com>
2025-01-08 19:29:54 -0500

    Moved scrape logic and filtering to new base filter and sub filter classes

--- spider.py
+++ spider.py
@@ -1,17 +1,13 @@
-from bs4 import BeautifulSoup
 from threading import Lock
 import sqlite3
 import time
 
-#nltk.download('stopwords')
-
 class Spider:
 
     en = None
-    r = None
 
     def __init__(self, engine):
-        print("Creating Spider")
+        
         self.url = ''
         self.lock = Lock()
         Spider.en = engine
@@ -30,41 +26,36 @@ class Spider:
             WebData = Spider.request_work(spider_num) # request string of HTML text
             HTML = WebData[0]
             url = WebData[1]
-            data = Spider.crawl(HTML, anchor)
+
+            if not HTML or Spider.en.already_crawled(url, crawler_conn):
+                continue
+            
+            data = Spider.crawl(self, HTML, anchor)
             with self.lock:
                 Spider.en.export_scraped(data, url, scheduler_conn, crawler_conn)
 
         scheduler_conn.close()
         crawler_conn.close()
 
-    def crawl(html_input : str, anchor : str):
-        soup = BeautifulSoup(html_input, 'html.parser')
-        
-        # Title of the webpage
-        title = (soup.title.string).replace(" - Wikipedia", "") if soup.title else "No title found"
-        
-        # Clean data to analyze
-        catlinks_div = soup.find('div', id='mw-normal-catlinks')
-        content_div = soup.find('div', id='mw-content-text')
-        # Keywords
-        content_text = ""
-        if content_div:
-            paragraphs = content_div.find_all('p')  # Find all <p> tags
-            content_text = " ".join(p.get_text(strip=True) for p in paragraphs)
-        
-        # Get data
-        catlinks = Spider.en.filter.get_catlinks(catlinks_div, anchor)
-        content_links = Spider.en.filter.get_content_links(content_div, anchor)
+    # Rework crawling and link gathering for generality.
+    # Move soup to filter, use tuples for all possible types of content to find
 
-        # Get keywords
-        keywords_events = Spider.en.filter.get_keywords_and_events(content_text)
+    def crawl(self, html_input : str, anchor : str):
 
-        # Return the scraped data
-        data = (title, catlinks, content_links, keywords_events)
-        return data
+        # Collect divs to analyze
+        content_divs = Spider.en.filter.get_divs(html_input)
+        if not content_divs:
+            print(f"No content divisions found for anchor {anchor}")
+            return ([],[],[]
+                    )
+        # Returns a tuple of lists:  (Links, KeyWords, KeyEvents)
+        links_keywords_keyevents = Spider.en.filter.extract_data(content_divs, Spider.en.anchor)
+
+        return links_keywords_keyevents
 
 
     @staticmethod
     def request_work(spider_num : str):
         HTML = Spider.en.schedule_a_spider(spider_num)
         return HTML
+    
