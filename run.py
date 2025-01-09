7935a11d52afb7dd6c1238971393cc945e60ddb5
Logan Jarvis <ljarvis2@rocketmail.com>
2025-01-08 19:30:39 -0500

    Added new potential Britannica start

--- run.py
+++ run.py
@@ -1,13 +1,18 @@
 from engine import Engine
 from Filters.wikipedia_f import WikipediaFilter
+from Filters.base_f import BaseFilter
 
 wikipedia_info = ("https://en.wikipedia.org/wiki/Eggs_as_food", "https://en.wikipedia.org/")
-filter = WikipediaFilter()
+britannica_info = ("https://www.britannica.com/money/on-track-retirement-savings", "https://www.britannica.com/")
+
+filter = BaseFilter()
+# filter = WikiFilter()
 thread_number = 8
 
 # Ensure that this info is correct and corresponds the anchor website
 if __name__ == "__main__":
-    
-    Engine(wikipedia_info, 1, filter, thread_number)
 
-    print("\nengine fully initalize, program completed.")
+    
+    Engine(britannica_info, 5, filter, thread_number)
+
+    print("\nEngine fully initalize... Program completed")
